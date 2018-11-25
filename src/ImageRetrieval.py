4894b4fd90a492b3c06d0dd2923b1025bda8758d
Rhys <DFGRhys@gmail.com>
2018-11-25 20:31:56 +0000

    More documentation

--- src/ImageRetrieval.py
+++ src/ImageRetrieval.py
@@ -13,6 +13,9 @@ import cv2
 
 class Camera(object):
     def __init__(self):
+        """
+        The Camera object is used to maintain the video feed to capture web-cam images.
+        """
         self.__cap = cv2.VideoCapture(0)
 
         if not self.__cap.isOpened():
@@ -22,12 +25,24 @@ class Camera(object):
         self.__cap_y_size = self.__cap.get(4)
 
     def capture_image(self):
+        """
+        Capture an image from the video feed.
+        :return: OpenCV2 image from video feed.
+        """
         ret, frame = self.__cap.read()
         return frame
 
     def get_image_size(self):
+        """
+        Get the image size of the images from this video feed.
+        :return: (x, y, ) for the size of the images from this video feed.
+        """
         return self.__cap_x_size, self.__cap_y_size
 
     def close_feed(self):
+        """
+        Close down the video feed.
+        :return None:
+        """
         # todo: Complete this method
         pass
